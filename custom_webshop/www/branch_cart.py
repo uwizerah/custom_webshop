8497defe5628ca432b39c448f11bff94e28921a9
uwizerah <u.kabasinga@gmail.com>
2025-04-16 15:22:47 +0200

    fix: Ignore submitted or empty carts

--- custom_webshop/www/branch_cart.py
+++ custom_webshop/www/branch_cart.py
@@ -3,5 +3,11 @@ from webshop.webshop.shopping_cart.cart import get_cart_quotation
 
 def get_context(context):
     cart_data = get_cart_quotation()
-    context.doc = cart_data["doc"]
-    context.cart_settings = cart_data["cart_settings"]
+    quotation = cart_data.get("doc")
+
+    if not quotation or quotation.get("docstatus") != 0 or not quotation.items:
+        context.doc = None
+    else:
+        context.doc = quotation
+
+    context.cart_settings = cart_data.get("cart_settings")
